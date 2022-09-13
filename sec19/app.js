60ea4ec5f72a357b20c5eb2ff4fdcb483ad5f33c
Uzair Vawda <uzair.vawda@gmail.com>
2022-09-13 16:02:58 -0400

    Completed base get and post routes, added EJS

--- sec19/app.js
+++ sec19/app.js
@@ -1,48 +1,51 @@
-const fs = require('fs')
-const path = require('path')
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
 
 const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs' );
+
 app.use(express.static('public'));
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({extended: false}));
 
 app.get('/', function(req, res, next) {
-	res.sendFile(path.join(__dirname, 'views', 'index.html'));
+	res.render('index');
 });
 
 app.get('/restaurants', function(req, res, next) {
-	res.sendFile(path.join(__dirname, 'views', 'restaurants.html'));
-});
-
-app.get('/recommend', function(req, res, next) {
-	res.sendFile(path.join(__dirname, 'views', 'recommend.html'));
-});
-
-app.get('/confrim', function(req, res, next) {
-	console.log(__dirname)
-	res.sendFile(path.join(__dirname, 'views', 'confrim.html'));
-});
-
-app.get('/about', function(req, res, next) {
-	res.sendFile(path.join(__dirname, 'views', 'about.html'));
-});
-
-app.post('/recommendRes', function(req, res, next) {
 	const filePath = path.join(__dirname, 'data', 'recs.json')
 	const fileData = fs.readFileSync(filePath)
 	const fileDataJSON = JSON.parse(fileData)
-	const recommend = {
-		name: req.body.name,
-		address: req.body.address,
-		cuisine: req.body.cuisine,
-		website: req.body.website,
-		description: req.body.description
-	}
-	fileDataJSON.push(recommend)
-	console.log(recommend)
+
+	res.render('restaurants', {
+		data: fileDataJSON,
+		numberofRes: fileDataJSON.length
+	});
+});
+
+app.get('/recommend', function (req, res) {
+  res.render('recommend');
+});
+
+app.post('/recommendRes', function(req, res, next) {
+	const restaurant = req.body;
+	const filePath = path.join(__dirname, 'data', 'recs.json')
+	const fileData = fs.readFileSync(filePath)
+	const fileDataJSON = JSON.parse(fileData)
+
+	fileDataJSON.push(restaurant)
 	fs.writeFileSync(filePath, JSON.stringify(fileDataJSON));
-	// res.send('Data Stored!')
-	res.redirect('/restaurants');
+	res.redirect('/confirm');
 })
+
+app.get('/confirm', function (req, res) {
+  res.render('confirm');
+});
+
+app.get('/about', function (req, res) {
+  res.render('about');
+});
 
 app.listen(6969)
